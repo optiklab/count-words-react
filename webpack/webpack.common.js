5c4286b91baff50565a136238c41f040e84376bb
Anton Yarkov <byslash@gmail.com>
2024-07-10 12:29:15 +0200

    Updates

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,7 +1,8 @@
-const webpack = require("webpack");
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const srcDir = path.join(__dirname, "..", "src");
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const srcDir = path.join(__dirname, '..', 'src');
 
 module.exports = {
     entry: {
@@ -11,34 +12,57 @@ module.exports = {
       contentScript: path.join(srcDir, 'content-script.tsx'),
       wordsCounter: path.join(srcDir, 'words-counter.ts')
     },
-    output: {
-        path: path.join(__dirname, "../dist/js"),
-        filename: "[name].js",
-    },
-    optimization: {
-        splitChunks: {
-            name: "vendor",
-            chunks(chunk) {
-              return chunk.name !== 'background';
-            }
-        },
-    },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: "ts-loader",
+                use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+              test: /\.css$/i,
+              use: ['style-loader', 'css-loader'],
+            },
+            {
+              test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+              type: 'asset/resource'
+            }
         ],
     },
     resolve: {
         extensions: [".ts", ".tsx", ".js"],
     },
     plugins: [
+        new CleanWebpackPlugin({
+          cleanStaleWebpackAssets: false,
+        }),
         new CopyPlugin({
             patterns: [{ from: ".", to: "../", context: "public" }],
             options: {},
         }),
+        ...getHtmlPlugins([
+          'popup',
+          'options'
+        ]),
     ],
-};
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, '../dist/js')
+    },
+    optimization: {
+      splitChunks: {
+        chunks(chunk) {
+          return chunk.name !== 'contentScript' && chunk.name !== 'background'
+        }
+      },
+    }
+}
+
+function getHtmlPlugins(chunks) {
+  return chunks.map(chunk => new HtmlPlugin({
+    title: 'React Extension',
+    filename: `${chunk}.html`,
+    chunks: [chunk],
+  }))
+}
+
